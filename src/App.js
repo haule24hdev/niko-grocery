521bf2affd0102236fcab31efe65968e2f1dae4f
haule <haule.24hdev@gmail.com>
2019-11-19 10:12:58 +0700

    intergated redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { Provider } from "react-redux";
 import AppRouter from "AppRouter";
-import 'antd/dist/antd.css';
+import store from "redux/store";
+
+import "antd/dist/antd.css";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <Provider store={store}>
       <AppRouter />
-    </div>
+    </Provider>
   );
 }
 
---
+++ src/redux/root-reducer.js
@@ -0,0 +1,14 @@
+import { combineReducers } from "redux";
+
+function counterReducer(state = 0, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+}
+const rootReducer = combineReducers({counter: counterReducer})
+export default rootReducer;
